90173f100d590eb6ff6936b11f5cd8ada36fff6b
totohazima <najaehwan123@gmail.com>
2024-01-16 10:06:14 +0900

    no message

---
+++ Assets/Script/Player/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("STAT")]
+    public int level;
+    public float maxHealth;
+    public float regeneration;
+    public float bloodSucking;
+    public float persentDamage;
+    public float meleeDamage;
+    public float rangeDamage;
+    public float attackSpeed;
+    public float criticalChance;
+    public float engine;
+    public float range;
+    public float armor;
+    public float evasion;
+    public float accuracy;
+    public float speed;
+}
---
+++ Assets/Script/Weapon/Weapon.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class Weapon
+{
+    public enum Weapons
+    {
+        PISTOL,
+        SHOTGUN,
+        DOUBLESHOTGUN,
+        SPEAR,
+        SHREDDER,
+        PUNCH,
+        RENCH,
+        DRIVER
+    }
+}
